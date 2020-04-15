6e55eeb5ee1f03ba4c078ccb0b866dcb2c4bd579
Remy Hunter <remyhunter256@gmail.com>
2020-04-15 12:32:11 -0400

    Add Device orientation control

--- data.js
+++ data.js
@@ -2,7 +2,7 @@ var APP_DATA = {
   "scenes": [
     {
       "id": "0-image1",
-      "name": "image1",
+      "name": "Electroknit Dymaxion VR View",
       "levels": [
         {
           "tileSize": 256,
@@ -27,23 +27,34 @@ var APP_DATA = {
         "yaw": 2.5012423609771073,
         "pitch": -0.2115588756214528,
         "fov": 1.5707963267948966
-      },
+      }, // {
+        //   "yaw": 1.4697767647941031,
+        //   "pitch": -0.3273774728262282,
+        //   "rotation": 0,
+        //   "target": "0-image1"
+        // }
       "linkHotspots": [
-        {
+        // {
+        //   "yaw": 1.4697767647941031,
+        //   "pitch": -0.3273774728262282,
+        //   "rotation": 0,
+        //   "target": "0-image1"
+        // }
+      ],
+      "infoHotspots": [
+       {
           "yaw": 1.4697767647941031,
           "pitch": -0.3273774728262282,
-          "rotation": 0,
-          "target": "0-image1"
-        }
-      ],
-      "infoHotspots": []
+          "title": "Example Tile",
+          "text": "This is an example info bit"
+        }]
     }
   ],
-  "name": "electroknit dymaxion",
+  "name": "electroknit-dymaxion",
   "settings": {
     "mouseViewMode": "qtvr",
     "autorotateEnabled": true,
-    "fullscreenButton": false,
+    "fullscreenButton": true,
     "viewControlButtons": false
   }
 };
