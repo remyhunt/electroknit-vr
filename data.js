9abd01de40d93def60d7a9eaf888d8424750073b
Remy Hunter <remyhunter256@gmail.com>
2020-04-15 11:56:19 -0400

    Initial Commit

---
+++ data.js
@@ -0,0 +1,49 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-image1",
+      "name": "image1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": 2.5012423609771073,
+        "pitch": -0.2115588756214528,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.4697767647941031,
+          "pitch": -0.3273774728262282,
+          "rotation": 0,
+          "target": "0-image1"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "electroknit dymaxion",
+  "settings": {
+    "mouseViewMode": "qtvr",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
